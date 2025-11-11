23c350cb47466e9c5f505a575a43ebbec2c75f21
Arreeya Suwanmosi <tanne270@gmail.com>
2025-11-11 15:18:15 +0900

    Rail Task Manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,49 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new #for form_with
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to "/tasks/#{@task.id}"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to "/tasks"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to "/tasks", status: :see_other
+  end
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,41 @@
+# Rails.application.routes.draw do
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+#   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+#   # Can be used by load balancers and uptime monitors to verify that the app is live.
+#   get "up" => "rails/health#show", as: :rails_health_check
+
+#   # Defines the root path route ("/")
+#   # root "posts#index"
+#   get "tasks", to: "tasks#index"
+#   get "tasks/:id", to: "tasks#show"
+#   post "tasks", to: "tasks#create"
+
+#   #create
+#   get "tasks/new", to:"tasks#new"
+#   post "tasks", to:"tasks#create"
+
+#  # UPDATE
+#   get   "tasks/:id/edit", to: "tasks#edit"
+#   patch "tasks/:id", to: "tasks#update"
+# end
+
+
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # READ all
+  get    "tasks", to: "tasks#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # CREATE
+  get    "tasks/new", to: "tasks#new"      # << ต้องมาก่อน :id
+  post   "tasks", to: "tasks#create"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # READ one
+  get    "tasks/:id", to: "tasks#show"
+
+  # UPDATE
+  get    "tasks/:id/edit", to: "tasks#edit"     # << ควรอยู่ก่อน patch :id เช่นกัน
+  patch  "tasks/:id", to: "tasks#update"
+
+  # DELETE
+  delete "tasks/:id", to: "tasks#destroy"
 end
